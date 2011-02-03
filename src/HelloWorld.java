f446f9524e6ae12a1c8a286d0605eea7bba365b5
Steven Totten <stotten70@gmail.com>
2011-02-02 23:08:53 -0500

    Steven's test commit project

---
+++ src/HelloWorld.java
@@ -0,0 +1,16 @@
+
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Hopefully this works, but it seems
+		//to be pretty easy to set up.  Let's
+		//see if I can push this stuff out
+		//correctly.  Wish me luck!!
+System.out.println("What's up team?  Steven's in the house!!!");
+	}
+
+}
